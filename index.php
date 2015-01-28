4e6f1eb6e40163c30ce77390f4cfcf0ea800a61f
Hans Kuijpers <hans2103@gmail.com>
2015-01-29 00:05:11 +0100

    step 7 - begin of index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+	defined( '_JEXEC' ) or die( 'Restricted access' );
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>" >
+	<head>
+		<jdoc:include type="head" />
+	</head>
+	<body>
+		Hello World!
+	</body>
+</html>
