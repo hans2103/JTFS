89ac1660e60f03e8ce991029da6e4fa8297dda16
Hans Kuijpers <hans2103@gmail.com>
2015-01-31 00:19:48 +0100

    fix for forgotten Bootstrap 3 styling

--- pagelayout/homepage.php
+++ pagelayout/homepage.php
@@ -2,7 +2,13 @@
 defined('_JEXEC') or die;
 ?>
 
-<?php if(count(JFactory::getApplication()->getMessageQueue())):?>
-	<jdoc:include type="message" />
-<?php endif; ?>
-<jdoc:include type="component" />
+<div class="container">
+	<div class="row">
+		<div class="col-xs-12">
+			<?php if(count(JFactory::getApplication()->getMessageQueue())):?>
+			<jdoc:include type="message" />
+			<?php endif; ?>
+			<jdoc:include type="component" />
+		</div>
+	</div>
+</div>
